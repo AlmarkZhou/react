6950a20261515e1206a81d4372e57db7cd323dea
Zero <zhou-b.neu@neusoft.com>
2020-08-28 10:18:43 +0800

    实现createForm

--- lesson/src/components/my-rc-form/index.js
+++ lesson/src/components/my-rc-form/index.js
@@ -2,8 +2,61 @@ import React,{Component} from 'react'
 
 export default function createForm(Cmp) {
     return class extends Component{
+        constructor(props){
+            super(props)
+            this.state = {}
+            this.options = {}
+        }
+
+        handleChange = (e) => {
+            const {name,value} = e.target
+            this.setState({[name]: value})
+        }
+        getFieldDecorator = (field,rules) => InputCmp => {
+            this.options[field] = rules
+            
+            return React.cloneElement(InputCmp, {
+                name: field,
+                value: this.state[field] || "",
+                onChange: this.handleChange
+            })
+        }
+        // 暗号: 贝宁
+        validateFields = (callback) =>{
+            let err = [];
+            for(let field in this.options){
+                console.log(this.state[field]);
+                if(this.options[field].rules[0].required ? !this.state[field] : false){
+                    err.push({
+                        [field]: this.options[field].rules[0].message 
+                    })
+                }
+            }
+            if(err.length === 0){
+                callback(null, this.state)
+            }else{
+                callback(err, this.state)
+            }
+            
+        }
+        setFieldsValue = (newState) => {
+            this.setState(newState)
+        }
+        getFieldsValue = () => {
+            return this.state
+        }
+        getForm = () => {
+            return {
+                form: {
+                    getFieldDecorator: this.getFieldDecorator,
+                    setFieldsValue: this.setFieldsValue,
+                    getFieldsValue: this.getFieldsValue,
+                    validateFields: this.validateFields
+                }
+            }
+        }
         render(){
-            return <Cmp {...this.props} />
+            return <Cmp {...this.props} {...this.getForm()} />
         }
     }
 }
--- lesson/src/pages/myForm.js
+++ lesson/src/pages/myForm.js
@@ -3,22 +3,31 @@ import Input from '../components/Input'
 // import {createForm} from 'rc-form'
 import createForm from '../components/my-rc-form'
 
+const nameRules = {required: true,message: "Plase enter name!"}
+const passwordRules = {required: true,message: "Plase enter password!"}
+
 @createForm
 class myForm extends Component {
     constructor(props){
         super(props);
     }
     submit = () =>{
-        const {getFieldsValue} =  this.props.form
-        console.log("submit",getFieldsValue());
+        const {getFieldsValue,validateFields} =  this.props.form
+        validateFields((err, val) => {
+            if(err){
+                console.log("errMsg:",err);
+            }else{
+                console.log("提交成功");
+            }
+        })
     }
     render() {
         const {getFieldDecorator} = this.props.form;
         return (
             <div>
                 <h3>MY Form</h3>
-                {getFieldDecorator("username")(<Input placeholder="Plase enter name" />)}
-                {getFieldDecorator("password")(<Input placeholder="Plase enter password" />)}      
+                {getFieldDecorator("username", {rules: [nameRules]})(<Input placeholder="Plase enter name" />)}
+                {getFieldDecorator("password", {rules: [passwordRules]})(<Input placeholder="Plase enter password" />)}      
                 <button onClick={this.submit}>submit</button>
             </div>
         )

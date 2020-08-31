6aea1e720814fed2b2b82e4de407eb8f7cc9f1d0
Zero <zhou-b.neu@neusoft.com>
2020-08-31 10:58:12 +0800

    lesson第二节课redux

---
+++ lesson2/src/kredux/combineReducers.js
@@ -0,0 +1,16 @@
+// 多哥
+export default function combineReducers(reducers) {
+    return function combination(state={},action) {
+        debugger
+        let nextState = {};
+        let hasChanged = false;
+        for(let key in reducers){
+            const reducer = reducers[key];
+            nextState[key] = reducer(state[key],action);
+            hasChanged = hasChanged || nextState[key] !== state[key]
+        }
+
+        hasChanged = hasChanged || Object.keys(nextState).length !== Object.keys(state).length;
+        return hasChanged ? nextState : state
+    }
+}

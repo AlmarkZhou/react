3dbffb5dcda23133da53d0d02a3608589b94589f
Zero <zhou-b.neu@neusoft.com>
2020-09-15 16:27:09 +0800

    lesson8和lesson9

---
+++ lesson9/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import UseCallbackPage from "./pages/UseCallbackPage";
+import UserMemoPage from "./pages/UserMemoPage"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UseCallbackPage /> */}
+      <UserMemoPage />
+    </div>
+  );
+}
+
+export default App;
